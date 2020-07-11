df08de9e27016ad83dcce64992af98626ea3fe25
Ryan <devogelryan@gmail.com>
2020-07-11 21:27:13 +0200

    Second commit

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+import random
+
 Family = ["Ryan", "Laura", "Erwin", "Peggy"]
 
 for x in Family:
 	print(x)
+
+notGuessed = True
+maxBound = 5
+
+while notGuessed:
+	userInput = input("Raad een getal tussen 0-5")
+	NumberToGuess = random.randint(0, 5)
+	print(NumberToGuess)
+	if userInput.isdiggit():
+		if(userInput == NumberToGuess):
+			print("You won!")
+			notGuessed = False
+			
+
